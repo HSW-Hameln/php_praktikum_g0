88f6a0b1c92f0632e5294f3a74de8b5e0f07aeca
smielke <mielke@hsw-hameln.de>
2014-05-20 11:54:06 +0200

    example

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mielke
+ * Date: 20.05.14
+ * Time: 10:44
+ *
+ * Diese Datei wird auf dem Server Automatisch überschrieben.
+ */
+
+$DB_HOST = 'localhost';
+$DB_USER = 'root';
+$DB_PASS = '';
+$DB_NAME = 'DB_G0';
